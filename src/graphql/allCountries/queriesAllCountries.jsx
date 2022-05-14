4892cace29326f552c08b4434acaabbed4f2d8e7
heredialucas <heredialucasfac22@hotmail.com>
2022-05-14 12:20:52 -0300

    Fix: Test graphql function

--- src/graphql/allCountries/queriesAllCountries.jsx
+++ src/graphql/allCountries/queriesAllCountries.jsx
@@ -2,9 +2,33 @@ import { gql } from '@apollo/client'
 
 export const ALL_COUNTRIES = gql`
 	query allCountries {
-		countries {
-			name
+		continents {
 			code
+			name
+		}
+		languages {
+			code
+			name
+		}
+		countries {
+			code
+			name
+			native
+			phone
+			capital
+			currency
+			emoji
+			states {
+				name
+			}
+			continent {
+				code
+				name
+			}
+			languages {
+				code
+				name
+			}
 		}
 	}
 `
